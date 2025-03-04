3e22b5949f488aaa852e697050596485cc0ef844
ManoloVM <mvasquezmendoza@byupathway.edu>
2025-03-04 10:01:07 -0500

    exercise3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomgenerator = new Random();
+        int number = randomgenerator.Next(1,101);
+
+        string usern;
+        int guess;
+        int attempts = 0;
+        string response = "yes";
+        while (response == "yes")
+        {
+            do{
+                Console.Write("What is your guess? ");
+            usern = Console.ReadLine();
+            guess = int.Parse(usern);
+            
+            if (guess == number)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else{
+                Console.WriteLine("Lower");
+            }
+
+            attempts=attempts+1;
+            
+            } while (guess != number);
+            Console.WriteLine($"You did it in {attempts} attempts.");
+            Console.WriteLine("Do you want to play again? ");
+            response = Console.ReadLine();
+        }
+        
     }
 }

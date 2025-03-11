e58a1d86911b6f36dd5579f806744d415583800a
ManoloVM <mvasquezmendoza@byupathway.edu>
2025-03-11 07:08:08 -0500

    final aggregates

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2019;
+        job1._endYear = 2025;
+
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Software Assistant";
+        job2._startYear = 2016;
+        job2._endYear = 2019;
+
+        Resume presume = new Resume();
+        presume._name = "Joseph Gardner";
+        presume._jobs.Add(job1);
+        presume._jobs.Add(job2);
+        Console.WriteLine(presume._jobs[0]._jobTitle);
+        presume.Display();
+
     }
 }
